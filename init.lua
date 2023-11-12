70607e299d9de871970b1c55606ac9223d39fd1c
tyler <tmccown21@gmail.com>
2023-11-12 13:15:21 -0800

    Window splitting and navigation mappings

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
 -- My conquest begins to reconfigure nvim in lua from scratch
 
-local g = vim.g         -- Global varaibles
-local opt = vim.opt     -- Set options
+local g = vim.g             -- Global variables
+local opt = vim.opt         -- Options
+local keymap = vim.keymap   -- Set keybindings
 
 -- Leader is space
 g.mapleader = ' '
@@ -15,4 +16,15 @@ opt.tabstop = 4         -- 1 tab = 4 spaces
 -- Search and highlighting
 opt.smartcase = true
 opt.hls = true
-vim.keymap.set('n', '<leader>hh', ':set hls!<cr>', {silent = true})
+keymap.set('n', '<leader>hh', ':set hls!<cr>', {silent = true})
+
+-- Split windows the way I expect them
+opt.splitbelow = true
+opt.splitright = true
+
+-- Window navigation
+keymap.set('n', '<c-h>', '<c-w>h', {silent = true})
+keymap.set('n', '<c-j>', '<c-w>j', {silent = true})
+keymap.set('n', '<c-k>', '<c-w>k', {silent = true})
+keymap.set('n', '<c-l>', '<c-w>l', {silent = true})
+keymap.set('n', '<leader>wq', ':windo wq<cr>', {silent = true})
