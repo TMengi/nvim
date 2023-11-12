ad9465b6dd92f5461c8c3da409f3bd486d7759ea
tyler <tmccown21@gmail.com>
2023-11-12 15:36:33 -0800

    More editor options and TODOs

--- init.lua
+++ init.lua
@@ -1,5 +1,14 @@
 -- My conquest begins to reconfigure nvim in lua from scratch
 
+-- TODO
+-- [ ] configure tabwidth by file extension
+--   - lua should be 2
+-- [ ] language server with mason
+-- [ ] leap.nvim
+-- [ ] gitgutter or gitlens
+-- [ ] commenter
+-- [ ] indentline
+
 local g = vim.g     -- Global variables
 
 -- Leader is space
--- lua/core/editor.lua
+++ lua/core/editor.lua
@@ -18,11 +18,15 @@ keymap.set('n', '<leader>hh', ':set hls!<cr>', {silent = true})
 opt.splitbelow = true
 opt.splitright = true
 
--- Window navigation
+-- Window navigation and resizing
 keymap.set('n', '<c-h>', '<c-w>h', {silent = true})
 keymap.set('n', '<c-j>', '<c-w>j', {silent = true})
 keymap.set('n', '<c-k>', '<c-w>k', {silent = true})
 keymap.set('n', '<c-l>', '<c-w>l', {silent = true})
+keymap.set('n', '<up>', ':resize +1<cr>', {silent = true})
+keymap.set('n', '<down>', ':resize -1<cr>', {silent = true})
+keymap.set('n', '<left>', ':vertical resize -1<cr>', {silent = true})
+keymap.set('n', '<right>', ':vertical resize +1<cr>', {silent = true})
 keymap.set('n', '<leader>wq', ':windo wq<cr>', {silent = true})
 
 -- Line numbering
@@ -32,6 +36,5 @@ opt.number = true
 opt.textwidth = 79
 opt.colorcolumn = '80,120'
 
--- TODO
--- [ ] configure tabwidth by file extension
---   - lua should be 2
+-- Make the cmd window taller for displaying errors
+opt.cmdheight = 2
