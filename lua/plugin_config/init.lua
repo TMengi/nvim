57b994849bcdf642b3c167836775c161ed97baf2
tyler <tmccown21@gmail.com>
2023-11-12 15:47:23 -0800

    Simplify plugin file managing

--- lua/plugin_config/init.lua
+++ lua/plugin_config/init.lua
@@ -1,6 +1,15 @@
 -- Allows us to wholesale require('plugin_config') in the main init.lua
-require('plugin_config.gruvbox')
-require('plugin_config.lualine')
-require('plugin_config.nvim-tree')
-require('plugin_config.nvim-treesitter')
-require('plugin_config.telescope')
+
+-- Put new files in here, .lua config paths relative to config_dir
+local config_dir = 'plugin_config'
+plugin_names = {
+  'gruvbox',
+  'lualine',
+  'nvim-tree',
+  'nvim-treesitter',
+  'telescope',
+}
+
+for _, name in pairs(plugin_names) do
+  require(config_dir .. '.' .. name)
+end
