b404e97167077f5515f6b5951ab51343012575b0
Tyler McCown <tyler@astranis.com>
2023-11-20 15:25:43 -0800

    Move things around in lualine

--- lua/plugin_config/lualine.lua
+++ lua/plugin_config/lualine.lua
@@ -4,15 +4,23 @@ require('lualine').setup({
     theme = 'gruvbox', -- Should I be using 'auto'?
   },
   sections = {
-    lualine_b = {
-      'diff',
-      'diagnostics',
-    },
-    lualine_c = {
+    lualine_a = {
       {
         'filename',
         path = 1,
       },
     },
+    lualine_b = {
+      'diff',
+      {
+        'diagnostics',
+        sources = {
+          'nvim_diagnostic',
+          'nvim_lsp',
+        },
+      },
+    },
+    lualine_c = {},
   },
+  extensions = { 'nvim-tree' },
 })
