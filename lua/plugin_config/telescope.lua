14257f2533734e9710add8ae77aff8f61c3c6d35
Tyler McCown <tyler@astranis.com>
2024-02-23 12:01:08 -0800

    Use leader for telescope shortcuts. Also unbind <c-p> since I'm using zellij now

--- lua/plugin_config/telescope.lua
+++ lua/plugin_config/telescope.lua
@@ -3,9 +3,9 @@ local keymap = vim.keymap
 local builtin = require('telescope.builtin')
 
 local opts = { noremap = true }
-keymap.set('n', '<c-p>', builtin.find_files, opts)
-keymap.set('n', '<space><space>', builtin.oldfiles, opts)
-keymap.set('n', '<space>fg', builtin.live_grep, opts)
-keymap.set('n', '<space>fh', builtin.help_tags, opts)
-keymap.set('n', '<space>g', builtin.diagnostics, opts)
+keymap.set('n', '<leader>p', builtin.find_files, opts)
+keymap.set('n', '<leader><leader>', builtin.oldfiles, opts)
+keymap.set('n', '<leader>fg', builtin.live_grep, opts)
+keymap.set('n', '<leader>fh', builtin.help_tags, opts)
+keymap.set('n', '<leader>g', builtin.diagnostics, opts)
 keymap.set('n', '<leader>cto', builtin.quickfix, opts)
